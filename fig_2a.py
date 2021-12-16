fa7b0e11af61ae968388b6dcd4a5ce9a9364f183
Han Kim <hankim@caltech.edu>
2021-12-16 11:58:46 -0800

    new figs for NEW draft

--- fig_2a.py
+++ fig_2a.py
@@ -3,21 +3,25 @@ import bokeh.plotting
 import bokeh.palettes
 import bokeh.io
 
-x = np.linspace(0.01, 100, 50000)
+
+m = np.linspace(0.01, 100, 50000)
 
 p = bokeh.plotting.figure(height=500, 
                           width=600,
                           x_axis_type="log",
-                          x_axis_label="relative mutation rate towards advantageous allele (m)",
-                          y_axis_label="Probability of advantageous allele")
+                          x_axis_label="mu of a to A / mu of A to a",
+                          y_axis_label="probability of advantageous allele")
 
 selecn_by_drifts = [np.inf, 3.0, 1.0, 0.1, 0.0]
 colours = bokeh.palettes.Blues256[::50]
 
 for selecn_by_drift, colour in zip(selecn_by_drifts, colours):
-    p.line(x,
-#            1/(1 + np.exp(-1 * np.exp(selecn_by_drift) * x)),
-           (1 - np.exp(-1*np.exp(selecn_by_drift)*0.5*x))/(1 - np.exp(-1*np.exp(selecn_by_drift)*x)),
+    
+    num = 1 - np.exp(-1*np.exp(selecn_by_drift)*m)
+    denom = 1 - np.exp(-1*np.exp(selecn_by_drift)*50000*m)
+    
+    p.line(m,
+           num / denom,
            color=colour,
            line_width=3,
            legend_label=f"{selecn_by_drift}")
