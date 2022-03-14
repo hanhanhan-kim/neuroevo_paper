275ac56847e8a39528ba5a8c1360fcbd658908c3
Han Kim <hankim@caltech.edu>
2022-03-14 12:40:19 -0700

    add all tentative final changes

--- fig_2a.py
+++ fig_2a.py
@@ -1,32 +1,45 @@
 import numpy as np
-import bokeh.plotting
+import matplotlib.pyplot as plt
 import bokeh.palettes
-import bokeh.io
 
 
+# Make it look more like Bokeh
+plt.style.use('default')
+plt.rcParams['figure.dpi'] = 150
+plt.rcParams['savefig.dpi'] = 300
+plt.rcParams.update({'font.sans-serif':'Helvetica'})
+plt.rcParams['axes.grid'] = True
+plt.rcParams['grid.linewidth'] = 0.5
+plt.rcParams['axes.linewidth']= 0.5
+plt.rcParams['figure.figsize'] = [6, 6]
+
+# Plotting params:
+fig, ax = plt.subplots()
+linewidth = 4
+
+# Data and equation params:
 m = np.linspace(0.01, 100, 50000)
-
-p = bokeh.plotting.figure(height=500, 
-                          width=600,
-                          x_axis_type="log",
-                          x_axis_label="mu of a to A / mu of A to a",
-                          y_axis_label="probability of advantageous allele")
-
-selecn_by_drifts = [np.inf, 3.0, 1.0, 0.1, 0.0]
+selecn_by_drifts = [3.0, 1.0, 0.1, 0.0]
 colours = bokeh.palettes.Blues256[::50]
 
-for selecn_by_drift, colour in zip(selecn_by_drifts, colours):
-    
-    num = 1 - np.exp(-1*np.exp(selecn_by_drift)*m)
-    denom = 1 - np.exp(-1*np.exp(selecn_by_drift)*50000*m)
-    
-    p.line(m,
-           num / denom,
-           color=colour,
-           line_width=3,
-           legend_label=f"{selecn_by_drift}")
-    
-p.legend.location = "center_right"
-p.legend.title = "2Ne s ="
+# Manually plot the case for m=inf, because np.inf is mishandled otherwise: 
+ax.plot(m, len(m)*[1], color=colours[0], linewidth=linewidth, label=r"$\infty$")
 
-bokeh.io.show(p)
+# Plot non-infinity 2Ngs values:
+non_inf_colours = colours[1::]
+for selecn_by_drift, colour in zip(selecn_by_drifts, non_inf_colours):
+
+    num = m * np.exp(selecn_by_drift)
+    denom = (1 + m * np.exp(selecn_by_drift))
+    
+    ax.plot(m, num/denom, color=colour, linewidth=linewidth, label=f"{selecn_by_drift}")
+
+# Stylings:
+ax.text(x=4.1, y=0.5, s=r"$P(A) = \frac{me^{2N_es}}{1+me^{2N_es}}$", fontsize=14)
+ax.set_xlabel(r"$m = \frac{\mu_{a \rightarrow A}}{\mu_{A \rightarrow a}}$", fontsize=14)
+ax.set_ylabel(r"Probability of advantageous allele, $P(A)$", fontsize=14)
+ax.set_xscale('log')
+ax.legend(loc='best', title=r"$2N_es$")
+
+# plt.savefig("assets/fig2a.svg")
+plt.savefig("assets/fig2a.pdf")
